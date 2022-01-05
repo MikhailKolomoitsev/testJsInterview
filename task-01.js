d86db6d61151fbe836de674adf2a14fec169d318
mihon.testosterone <mihon.testosterone@gmail.com>
2022-01-05 18:41:36 +0200

    done

---
+++ task-01.js
@@ -0,0 +1,41 @@
+// const array = ['test1', 'test2', 'test3'];
+
+// let result = array.reduce((acc, item, index) => {
+
+//     if (acc) {
+//         acc = { ...acc, [`${index}`]: item }
+//         return acc
+//     } else {
+//         acc = {
+//             ...acc, [`${index}`]: item
+//         }
+//         return acc
+//     }
+// }, {})
+
+// console.log(result);
+
+// // output = {
+// //     '0': 'test1',
+// //     '1': 'test2',
+// //     '2': 'test3',
+// // }
+
+const array = [{ color: 'white', id: 2 }, { color: 'black', id: 3 }, { color: 'black', id: 500}];
+
+let result = array.reduce((acc, item) => {
+        acc = { ...acc, [`${item.color}`]: {...acc[item.color], [`${item.id}`]: item  }}
+        return acc
+},{})
+
+console.log(result);
+
+// output = {
+//     'white': {
+//         '2': { color: 'white', id: 2 },
+//     },
+//     'black': {
+//         '3': { color: 'black', id: 3 },
+//      ‘500’: { color: ‘black’, id: 500 }
+//   }
+// }
